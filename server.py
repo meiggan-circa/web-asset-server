8a5b43d52f974b870bec9932b249b91694deccf2
meiggan-circa <m.circaa@gmail.com>
2025-12-01 16:00:14 +0800

    format code

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ from bottle import (
     template,
     abort,
     HTTPResponse,
-    route
+    route,
 )
 
 
@@ -38,12 +38,13 @@ def generate_token(timestamp, filename):
     This is for comparing to the client submited token.
     """
     timestamp = str(timestamp)
-    mac = hmac.new(settings.KEY.encode(), timestamp.encode() + filename.encode(), 'md5')
-    return ':'.join((mac.hexdigest(), timestamp))
+    mac = hmac.new(settings.KEY.encode(), timestamp.encode() + filename.encode(), "md5")
+    return ":".join((mac.hexdigest(), timestamp))
 
 
 class TokenException(Exception):
     """Raised when an auth token is invalid for some reason."""
+
     pass
 
 
@@ -61,12 +62,12 @@ def validate_token(token_in, filename):
     """
     if settings.KEY is None:
         return
-    if token_in == '':
+    if token_in == "":
         raise TokenException("Auth token is missing.")
-    if ':' not in token_in:
+    if ":" not in token_in:
         raise TokenException("Auth token is malformed.")
 
-    mac_in, timestr = token_in.split(':')
+    mac_in, timestr = token_in.split(":")
     try:
         timestamp = int(timestr)
     except ValueError:
@@ -75,7 +76,10 @@ def validate_token(token_in, filename):
     if settings.TIME_TOLERANCE is not None:
         current_time = get_timestamp()
         if not abs(current_time - timestamp) < settings.TIME_TOLERANCE:
-            raise TokenException("Auth token timestamp out of range: %s vs %s" % (timestamp, current_time))
+            raise TokenException(
+                "Auth token timestamp out of range: %s vs %s"
+                % (timestamp, current_time)
+            )
 
     if token_in != generate_token(timestamp, filename):
         raise TokenException("Auth token is invalid.")
@@ -98,12 +102,16 @@ def require_token(filename_param, always=False):
         @include_timestamp
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if always or request.method not in ('GET', 'HEAD') or settings.REQUIRE_KEY_FOR_GET:
-                params = request.forms if request.method == 'POST' else request.query
+            if (
+                always
+                or request.method not in ("GET", "HEAD")
+                or settings.REQUIRE_KEY_FOR_GET
+            ):
+                params = request.forms if request.method == "POST" else request.query
                 try:
                     validate_token(params.token, params.get(filename_param))
                 except TokenException as e:
-                    response.content_type = 'text/plain; charset=utf-8'
+                    response.content_type = "text/plain; charset=utf-8"
                     response.status = 403
                     return response
             return func(*args, **kwargs)
@@ -121,8 +129,9 @@ def include_timestamp(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
-        (result if isinstance(result, Response) else response) \
-            .set_header('X-Timestamp', str(get_timestamp()))
+        (result if isinstance(result, Response) else response).set_header(
+            "X-Timestamp", str(get_timestamp())
+        )
         return result
 
     return wrapper
@@ -136,11 +145,12 @@ def allow_cross_origin(func):
         try:
             result = func(*args, **kwargs)
         except HTTPResponse as r:
-            r.set_header('Access-Control-Allow-Origin', '*')
+            r.set_header("Access-Control-Allow-Origin", "*")
             raise
 
-        (result if isinstance(result, Response) else response) \
-            .set_header('Access-Control-Allow-Origin', '*')
+        (result if isinstance(result, Response) else response).set_header(
+            "Access-Control-Allow-Origin", "*"
+        )
         return result
 
     return wrapper
@@ -161,24 +171,20 @@ def make_hdfs_path(coll, thumb, filename=""):
     if filename:
         parts.append(filename)
 
-    hdfs_path = '/'.join(p.strip('/') for p in parts)
+    hdfs_path = "/".join(p.strip("/") for p in parts)
     # Ensure it starts with /
-    if not hdfs_path.startswith('/'):
-        hdfs_path = '/' + hdfs_path
+    if not hdfs_path.startswith("/"):
+        hdfs_path = "/" + hdfs_path
     return hdfs_path
 
 
 def hdfs_path_exists(hdfs_path):
-    response = requests.get(
-        f"{settings.HDFS_API}/exists?path={hdfs_path}"
-    )
+    response = requests.get(f"{settings.HDFS_API}/exists?path={hdfs_path}")
     return response.ok
 
 
 def create_hdfs_dir(hdfs_dir):
-    requests.post(
-        f"{settings.HDFS_API}/mkdir/", data={"path": hdfs_dir}
-    )
+    requests.post(f"{settings.HDFS_API}/mkdir/", data={"path": hdfs_dir})
 
 
 def stream_hdfs_file(hdfs_path):
@@ -191,11 +197,7 @@ def stream_hdfs_file(hdfs_path):
 
 
 def upload_to_hdfs(hdfs_path, file):
-    requests.post(
-        f"{settings.HDFS_API}/upload/",
-        data={'path':hdfs_path},
-        files=file
-    )
+    requests.post(f"{settings.HDFS_API}/upload/", data={"path": hdfs_path}, files=file)
 
 
 def resolve_file():
@@ -205,7 +207,7 @@ def resolve_file():
 
     Returns the relative path to the requested file in HDFS.
     """
-    thumb_p = (request.query['type'] == "T")
+    thumb_p = request.query["type"] == "T"
     collection = request.query.coll
     filename = request.query.filename
     orig_path = make_hdfs_path(collection, False, filename)
@@ -219,9 +221,9 @@ def resolve_file():
 
     root, ext = path.splitext(filename)
 
-    if mimetype in ('application/pdf', 'image/tiff'):
+    if mimetype in ("application/pdf", "image/tiff"):
         # use PNG for PDF thumbnails
-        ext = '.png'
+        ext = ".png"
 
     thumb_name = f"{root}_{scale}{ext}"
     thumb_path = make_hdfs_path(collection, True, thumb_name)
@@ -241,16 +243,20 @@ def resolve_file():
     local_orig = path.join(tmp, filename)
     local_thumb = path.join(tmp, thumb_name)
 
-    with open(local_orig, 'wb') as f:
+    with open(local_orig, "wb") as f:
         for chunk in stream_hdfs_file(orig_path):
             f.write(chunk)
 
     input_spec = local_orig
-    convert_args = ('-resize', f"{scale}x{scale}>")
+    convert_args = ("-resize", f"{scale}x{scale}>")
 
-    if mimetype == 'application/pdf':
-        input_spec += '[0]'  # only thumbnail first page of PDF
-        convert_args += ('-background', 'white', '-flatten')  # add white background to PDFs
+    if mimetype == "application/pdf":
+        input_spec += "[0]"  # only thumbnail first page of PDF
+        convert_args += (
+            "-background",
+            "white",
+            "-flatten",
+        )  # add white background to PDFs
 
     # Generate thumbnail locally then store in HDFS
     log(f"Scaling thumbnail to {scale}")
@@ -260,11 +266,12 @@ def resolve_file():
     except ErrorReturnCode:
         log("Thumbnail generation failed, using fallback")
 
-        if len(convert_args) > 2: convert_args = convert_args[:2]
+        if len(convert_args) > 2:
+            convert_args = convert_args[:2]
         convert(thumb_fallback, *(convert_args + (local_thumb,)))
 
     thumb_dir = make_hdfs_path(collection, True)
-    file = {'file': (local_thumb, open(local_thumb, "rb"))}
+    file = {"file": (local_thumb, open(local_thumb, "rb"))}
 
     if not hdfs_path_exists(thumb_dir):
         create_hdfs_dir(thumb_dir)
@@ -278,7 +285,7 @@ def resolve_file():
     return thumb_path
 
 
-@route('/static/<path:path>')
+@route("/static/<path:path>")
 def static(path):
     """Serve static files to the client. Primarily for Web Portal."""
     if not settings.ALLOW_STATIC_FILE_ACCESS:
@@ -286,65 +293,69 @@ def static(path):
     return static_file(path, root=settings.BASE_DIR)
 
 
-@route('/getfileref')
+@route("/getfileref")
 @allow_cross_origin
 def getfileref():
     """Returns a URL to the static file indicated by the query parameters."""
     if not settings.ALLOW_STATIC_FILE_ACCESS:
         abort(404)
-    response.content_type = 'text/plain; charset=utf-8'
-    return "http://%s:%d/static/%s" % (settings.HOST, settings.PORT,
-                                       pathname2url(resolve_file()))
+    response.content_type = "text/plain; charset=utf-8"
+    return "http://%s:%d/static/%s" % (
+        settings.HOST,
+        settings.PORT,
+        pathname2url(resolve_file()),
+    )
 
 
-@route('/fileget')
-@require_token('filename')
+@route("/fileget")
+@require_token("filename")
 def fileget():
     """Returns the file data of the file indicated by the query parameters."""
-    data = b''.join(stream_hdfs_file(resolve_file()))
+    data = b"".join(stream_hdfs_file(resolve_file()))
     download_name = request.query.downloadname
     if download_name:
-        download_name = quote(path.basename(download_name).encode('ascii', 'replace'))
-        response.set_header('Content-Disposition', f"inline; filename*=utf-8''{download_name}")
+        download_name = quote(path.basename(download_name).encode("ascii", "replace"))
+        response.set_header(
+            "Content-Disposition", f"inline; filename*=utf-8''{download_name}"
+        )
     return data
 
 
-@route('/fileupload', method='OPTIONS')
+@route("/fileupload", method="OPTIONS")
 @allow_cross_origin
 def fileupload_options():
     response.content_type = "text/plain; charset=utf-8"
-    return ''
+    return ""
 
 
-@route('/fileupload', method='POST')
+@route("/fileupload", method="POST")
 @allow_cross_origin
-@require_token('store')
+@require_token("store")
 def fileupload():
-    """Accept original file uploads and store them in HDFS
-    """
-    thumb_p = (request.forms['type'] == "T")
+    """Accept original file uploads and store them in HDFS"""
+    thumb_p = request.forms["type"] == "T"
     filename = request.forms.store
     collection = request.forms.coll
     hdfs_dir = make_hdfs_path(collection, thumb_p)
     store_path = make_hdfs_path(collection, thumb_p, filename)
 
     if thumb_p:
-        return 'Ignoring thumbnail upload!'
+        return "Ignoring thumbnail upload!"
 
     if not hdfs_path_exists(hdfs_dir):
         create_hdfs_dir(hdfs_dir)
 
     upload = list(request.files.values())[0]
-    file = {'file': (filename, upload.file, upload.content_type)}
-    
+    file = {"file": (filename, upload.file, upload.content_type)}
+
     upload_to_hdfs(store_path, file)
 
-    response.content_type = 'text/plain; charset=utf-8'
-    return 'Ok.'
+    response.content_type = "text/plain; charset=utf-8"
+    return "Ok."
 
 
-@route('/filedelete', method='POST')
-@require_token('filename')
+@route("/filedelete", method="POST")
+@require_token("filename")
 def filedelete():
     """Delete the file indicated by the query parameters. Returns 404
     if the original file does not exist. Any associated thumbnails will
@@ -359,11 +370,9 @@ def filedelete():
         abort(404)
 
     log(f"Deleting {orig_path}")
-    requests.post(
-        f"{settings.HDFS_API}/delete/", data={"path": orig_path}
-    )
+    requests.post(f"{settings.HDFS_API}/delete/", data={"path": orig_path})
 
-    prefix = filename.split('.att')[0]
+    prefix = filename.split(".att")[0]
     thumb_dir = requests.get(f"{settings.HDFS_API}/list?path={thumb_dir_path}").json()
 
     for path_info in thumb_dir["data"]:
@@ -374,12 +383,12 @@ def filedelete():
                 f"{settings.HDFS_API}/delete/", data={"path": path_info["path"]}
             )
 
-    response.content_type = 'text/plain; charset=utf-8'
-    return 'Ok.'
+    response.content_type = "text/plain; charset=utf-8"
+    return "Ok."
 
 
-@route('/getmetadata')
-@require_token('filename')
+@route("/getmetadata")
+@require_token("filename")
 def getmetadata():
     """Provides access to EXIF metadata."""
     collection = request.query.coll
@@ -390,7 +399,7 @@ def getmetadata():
     if not hdfs_path_exists(orig_path):
         abort(404)
 
-    data = b''.join(stream_hdfs_file(orig_path))
+    data = b"".join(stream_hdfs_file(orig_path))
     file = BytesIO(data)
 
     try:
@@ -399,59 +408,67 @@ def getmetadata():
         log("Error reading exif data.")
         tags = {}
 
-    if datatype == 'date':
+    if datatype == "date":
         try:
-            return str(tags['EXIF DateTimeOriginal'])
+            return str(tags["EXIF DateTimeOriginal"])
         except KeyError:
-            abort(404, 'DateTime not found in EXIF')
+            abort(404, "DateTime not found in EXIF")
 
     out = defaultdict(dict)
     for key, value in list(tags.items()):
         parts = key.split()
-        if len(parts) < 2: continue
+        if len(parts) < 2:
+            continue
+
         try:
-            v = str(value).decode('ascii', 'replace').encode('utf-8')
+            v = str(value).decode("ascii", "replace").encode("utf-8")
         except TypeError:
             v = repr(value)
 
         out[parts[0]][parts[1]] = str(v)
 
-    response.content_type = 'application/json'
-    result = [OrderedDict((('Name', key), ('Fields', value)))
-            for key, value in list(out.items())]
+    response.content_type = "application/json"
+    result = [
+        OrderedDict((("Name", key), ("Fields", value)))
+        for key, value in list(out.items())
+    ]
 
     return json.dumps(result, indent=4)
 
 
-@route('/testkey')
-@require_token('random', always=True)
+@route("/testkey")
+@require_token("random", always=True)
 def testkey():
     """If access to this resource succeeds, clients can conclude
     that they have a valid access key.
     """
-    response.content_type = 'text/plain; charset=utf-8'
-    return 'Ok.'
+    response.content_type = "text/plain; charset=utf-8"
+    return "Ok."
 
 
-@route('/web_asset_store.xml')
+@route("/web_asset_store.xml")
 @include_timestamp
 def web_asset_store():
     """Serve an XML description of the URLs available here."""
-    response.content_type = 'text/xml; charset=utf-8'
-    return template('web_asset_store.xml', host="%s:%d" % (settings.SERVER_NAME, settings.SERVER_PORT))
+    response.content_type = "text/xml; charset=utf-8"
+    return template(
+        "web_asset_store.xml",
+        host="%s:%d" % (settings.SERVER_NAME, settings.SERVER_PORT),
+    )
 
 
-@route('/')
+@route("/")
 def main_page():
-    return 'It works!'
+    return "It works!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from bottle import run
+
     run(
-        host='0.0.0.0',
+        host="0.0.0.0",
         port=settings.PORT,
         server=settings.SERVER,
         debug=settings.DEBUG,
-        reloader=settings.DEBUG
+        reloader=settings.DEBUG,
     )
